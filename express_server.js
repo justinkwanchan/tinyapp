586b390557312afb51bc130c5172a3c8d613995b
justinkwanchan <justinkwanchan@gmail.com>
2020-12-10 20:37:53 +0000

    Linted files and added README.md and docs folder for pictures

--- express_server.js
+++ express_server.js
@@ -38,13 +38,13 @@ const urlDatabase = {
 
 const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: bcrypt.hashSync("dishwasher-funk", 10)
   }
 };
